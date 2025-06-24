3ba507411e4e1b9834ff86556ddd428818f76eca
ExzosDigital <willrulli@exzos.com.br>
2025-06-24 00:58:41 -0300

    Add main entry point for ExzosOnlook

---
+++ apps/exzosonlook/src/index.ts
@@ -0,0 +1,20 @@
+// ExzosOnlook Main Entry Point
+
+// Theme & Styling
+export * from './styles/theme';
+export * from './styles/tokens';
+
+// Provider
+export * from './provider';
+
+// Components
+export * from './components';
+
+// Hooks
+export * from './hooks';
+
+// Utils
+export * from './utils';
+
+// Icons
+export * from './icons';
