60ecd58006731e58df08f0bc68b95ae029d17923
ExzosDigital <willrulli@exzos.com.br>
2025-06-24 00:58:57 -0300

    Add components barrel file for ExzosOnlook

---
+++ apps/exzosonlook/src/components/index.ts
@@ -0,0 +1,75 @@
+// Components Barrel File
+
+// Basic UI
+export * from './ui/Button';
+export * from './ui/Card';
+export * from './ui/Avatar';
+export * from './ui/Badge';
+export * from './ui/Input';
+export * from './ui/Select';
+export * from './ui/Checkbox';
+export * from './ui/Radio';
+export * from './ui/Switch';
+export * from './ui/Tooltip';
+export * from './ui/Tabs';
+export * from './ui/Alert';
+export * from './ui/Toast';
+export * from './ui/Spinner';
+export * from './ui/Progress';
+export * from './ui/Skeleton';
+
+// Layout
+export * from './layout/Box';
+export * from './layout/Flex';
+export * from './layout/Grid';
+export * from './layout/Container';
+export * from './layout/Stack';
+export * from './layout/Divider';
+export * from './layout/Spacer';
+
+// Navigation
+export * from './navigation/Navbar';
+export * from './navigation/Sidebar';
+export * from './navigation/Menu';
+export * from './navigation/Dropdown';
+export * from './navigation/Breadcrumb';
+export * from './navigation/Pagination';
+export * from './navigation/AppSwitcher';
+
+// Data Display
+export * from './data/Table';
+export * from './data/DataGrid';
+export * from './data/List';
+export * from './data/Tree';
+export * from './data/Calendar';
+export * from './data/Timeline';
+export * from './data/Chart';
+
+// Feedback
+export * from './feedback/Modal';
+export * from './feedback/Drawer';
+export * from './feedback/Popover';
+export * from './feedback/Dialog';
+export * from './feedback/Notification';
+
+// Forms
+export * from './form/Form';
+export * from './form/FormField';
+export * from './form/Label';
+export * from './form/ErrorMessage';
+export * from './form/SubmitButton';
+
+// Typography
+export * from './typography/Heading';
+export * from './typography/Text';
+export * from './typography/Paragraph';
+export * from './typography/Link';
+export * from './typography/Code';
+
+// ExzosVerse Specific
+export * from './exzos/ExzosHeader';
+export * from './exzos/ExzosFooter';
+export * from './exzos/ExzosNavbar';
+export * from './exzos/ExzosSidebar';
+export * from './exzos/ExzosCard';
+export * from './exzos/ExzosButton';
